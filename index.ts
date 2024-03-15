db5a335c98ffec4b98e6fd362458ffc29f2fac1d
Steve <stevedsimkins@gmail.com>
2024-03-15 16:17:03 -0400

    experimental feed

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
-import express, { Express,  } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
-import { getFarcasterUser } from "./utils/getFarcasterUser"
+import { getFarcasterUser } from "./utils/getFarcasterUser";
 import { SignedKeyRequest, CastBody } from "./types";
 import { hexToBytes } from "@noble/hashes/utils";
-import {  Message,
+import {
+  Message,
   NobleEd25519Signer,
   FarcasterNetwork,
   makeCastAdd,
-} from "@farcaster/core"
-import { getFeed, getFeedFromAPI } from "./utils/getFeed";
+} from "@farcaster/core";
+import { getFeed, getFeedFromAPI, getAlgoFeedFromAPI  } from "./utils/getFeed";
 import { getUserByFid } from "./utils/geUserByFid";
 
 dotenv.config();
@@ -17,8 +18,7 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 export const hubUrl = "https://hub.pinata.cloud/v1";
-export const apiUrl = "https://api.pinata.cloud/v3"
-
+export const apiUrl = "https://api.pinata.cloud/v3";
 
 app.use(express.json());
 
@@ -26,44 +26,57 @@ app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Express + TypeScript Farcaster Server");
 });
 
-app.get("/user",async (req: express.Request, res: express.Response) => {
+app.get("/user", async (req: express.Request, res: express.Response) => {
   const { userFid } = req.query;
-  if(!userFid){
-    res.status(400).json({error: "No FID provided"});
+  if (!userFid) {
+    res.status(400).json({ error: "No FID provided" });
   }
   const fid = parseInt(userFid as string);
   const user = await getUserByFid(fid);
-  if(user){
+  if (user) {
     res.json(user);
-  }
-  else{
-    res.status(500).json({error: "Failed to get user"});
+  } else {
+    res.status(500).json({ error: "Failed to get user" });
   }
 });
 
 app.get("/feed", async (req: express.Request, res: express.Response) => {
-  const {channel, pageToken, api} = req.query;
-  if(!channel){
-    res.status(400).json({error: "No channel provided"});
+  const { channel, pageToken, api } = req.query;
+  if (!channel) {
+    res.status(400).json({ error: "No channel provided" });
   }
-  if(!pageToken){
-    res.status(400).json({error: "No pageToken provided"});
+  if (!pageToken) {
+    res.status(400).json({ error: "No pageToken provided" });
   }
   try {
-    if(api) {
-      const castsFromApi = await getFeedFromAPI(channel, pageToken)
+    if (api) {
+      const castsFromApi = await getFeedFromAPI(channel, pageToken);
       return res.json(castsFromApi);
     } else {
-      const castsFromHub = await getFeed(channel as string, pageToken as string);
+      const castsFromHub = await getFeed(
+        channel as string,
+        pageToken as string,
+      );
       return res.json(castsFromHub);
-    }    
+    }
   } catch (error) {
-    res.status(500).json({error: error});
+    res.status(500).json({ error: error });
   }
 });
 
-app.get("/sign-in/poll", async (req: express.Request, res: express.Response) => {
-  const {pollingToken} = req.query;
+app.get("/algo", async (req: express.Request, res: express.Response) => {
+  try {
+    const castsFromApi = await getAlgoFeedFromAPI();
+    return res.json(castsFromApi);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+app.get(
+  "/sign-in/poll",
+  async (req: express.Request, res: express.Response) => {
+    const { pollingToken } = req.query;
     try {
       const fcSignerRequestResponse = await fetch(
         `https://api.warpcast.com/v2/signed-key-request?token=${pollingToken}`,
@@ -72,58 +85,60 @@ app.get("/sign-in/poll", async (req: express.Request, res: express.Response) => 
           headers: {
             "Content-Type": "application/json",
           },
-        }
+        },
       );
       const responseBody = (await fcSignerRequestResponse.json()) as {
         result: { signedKeyRequest: SignedKeyRequest };
       };
-      console.log(responseBody)
-      res.status(200).json({"state": responseBody.result.signedKeyRequest.state, "userFid": responseBody.result.signedKeyRequest.userFid});
-    }
-    catch (error) {
+      console.log(responseBody);
+      res
+        .status(200)
+        .json({
+          state: responseBody.result.signedKeyRequest.state,
+          userFid: responseBody.result.signedKeyRequest.userFid,
+        });
+    } catch (error) {
       res.status(500).json(error);
     }
-  }
+  },
 );
 
 app.post("/sign-in", async (req: express.Request, res: express.Response) => {
   try {
     const farcasterUser = await getFarcasterUser();
-    if(!farcasterUser) {
-      res.status(500).json({error: "Failed to sign in user"});
+    if (!farcasterUser) {
+      res.status(500).json({ error: "Failed to sign in user" });
     }
-    if(farcasterUser) {
+    if (farcasterUser) {
       res.json({
-        deepLinkUrl: farcasterUser?.signerApprovalUrl, 
+        deepLinkUrl: farcasterUser?.signerApprovalUrl,
         pollingToken: farcasterUser?.token,
         publicKey: farcasterUser?.publicKey,
         privateKey: farcasterUser?.privateKey,
       });
-    }
-    else{
-      res.status(500).json({error: "Failed to get farcaster user"});
+    } else {
+      res.status(500).json({ error: "Failed to get farcaster user" });
     }
   } catch (error) {
-    res.status(500).json({error: error});
+    res.status(500).json({ error: error });
   }
 });
 
-
-app.post("/message", async (req: express.Request, res: express.Response) => { 
-  const NETWORK = FarcasterNetwork.MAINNET; 
+app.post("/message", async (req: express.Request, res: express.Response) => {
+  const NETWORK = FarcasterNetwork.MAINNET;
   try {
     console.log(req.body);
     const SIGNER = req?.body?.signer;
     const rawFID = req?.body?.fid;
     const message = req?.body?.castMessage;
     const parentUrl = req?.body?.parentUrl;
-    const FID = parseInt(rawFID)
+    const FID = parseInt(rawFID);
 
-    if(!SIGNER) {
-      return res.status(401).json({error: "No signer provided"});
+    if (!SIGNER) {
+      return res.status(401).json({ error: "No signer provided" });
     }
-    if(!FID) {
-      return res.status(400).json({error: "No FID provided"});
+    if (!FID) {
+      return res.status(400).json({ error: "No FID provided" });
     }
 
     const dataOptions = {
@@ -142,9 +157,8 @@ app.post("/message", async (req: express.Request, res: express.Response) => {
       mentionsPositions: [],
     };
 
-    if(parentUrl.length > 0){
+    if (parentUrl.length > 0) {
       castBody["parentUrl"] = parentUrl;
-
     }
 
     const castAddReq: any = await makeCastAdd(
@@ -171,14 +185,12 @@ app.post("/message", async (req: express.Request, res: express.Response) => {
       return res.status(500).json({ error: "Failed to submit message" });
     } else {
       let hex = Buffer.from(castResult.hash).toString("hex");
-      return res.status(200).json({hex: hex});
+      return res.status(200).json({ hex: hex });
     }
-  
   } catch (error) {
     console.log(error);
     return res.json({ "server error": error });
   }
-
 });
 
 app.listen(port, () => {
--- utils/getFeed.ts
+++ utils/getFeed.ts
@@ -4,30 +4,36 @@ import { apiUrl, hubUrl } from "..";
 import { Cast, Embed } from "../types";
 
 export const getFeedFromAPI = async (channel: any, nextPage: any) => {
-    try {
-      const result = await fetch(
-        `${apiUrl}//farcaster/casts?channel=${channel}&pageLimit=200&pageToken=${nextPage}`, {
-          headers: {
-            'Authorization': `Bearer ${process.env.PINATA_JWT}`
-          }
-        }
-      );
-      const resultData = await result.json();
+  try {
+    const result = await fetch(
+      `${apiUrl}//farcaster/casts?channel=${channel}&pageLimit=200&pageToken=${nextPage}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.PINATA_JWT}`,
+        },
+      },
+    );
+    const resultData = await result.json();
 
-      const pageToken = resultData.data.next_page_token;
-      const casts = resultData.data.casts;
-      const simplifiedCasts = await Promise.all(
-        casts.filter((c: Cast) => !c.parent_hash).map(async (cast: Cast) => {
+    const pageToken = resultData.data.next_page_token;
+    const casts = resultData.data.casts;
+    const simplifiedCasts = await Promise.all(
+      casts
+        .filter((c: Cast) => !c.parent_hash)
+        .map(async (cast: Cast) => {
           const fname = cast.author.username;
           const pfp = cast.author.pfp_url;
-          const { embedUrl, embedCast } = cast.embeds.reduce((acc: any, embed: Embed) => {
-            if (embed.url) {
-              acc.embedUrl.push(embed);
-            } else if (embed.castId) {
-              acc.embedCast.push(embed);
-            }
-            return acc;
-          }, { embedUrl: [], embedCast: [] });
+          const { embedUrl, embedCast } = cast.embeds.reduce(
+            (acc: any, embed: Embed) => {
+              if (embed.url) {
+                acc.embedUrl.push(embed);
+              } else if (embed.castId) {
+                acc.embedCast.push(embed);
+              }
+              return acc;
+            },
+            { embedUrl: [], embedCast: [] },
+          );
           return {
             id: cast.hash,
             castText: cast.content,
@@ -36,53 +42,161 @@ export const getFeedFromAPI = async (channel: any, nextPage: any) => {
             username: fname,
             pfp: pfp,
             timestamp: cast.timestamp,
-            likes: cast?.reactions?.likes?.length || 0, 
-            recasts: cast?.reactions?.recasts?.length || 0
+            likes: cast?.reactions?.likes?.length || 0,
+            recasts: cast?.reactions?.recasts?.length || 0,
           };
         }),
-      );
-      return simplifiedCasts;
-    } catch (error) {
-      console.log(error);
-      return error;
-    }
+    );
+    return simplifiedCasts;
+  } catch (error) {
+    console.log(error);
+    return error;
   }
+};
 
-  export const getFeed = async (channel: string, nextPage: string) => {
-    try {
-      const result = await fetch(
-        `${hubUrl}/castsByParent?url=${channel}&pageSize=20&reverse=true&pageToken=${nextPage}`,
-      );
-      const resultData = await result.json();
-      const pageToken = resultData.nextPageToken;
-      const casts = resultData.messages;
-      const simplifiedCasts = await Promise.all(
-        casts.map(async (cast: any) => {
-          const fname = await getFnameFromFid(cast.data.fid);
-          const pfp = await getPfpFromFid(cast.data.fid);
-          const { embedUrl, embedCast } = cast.data.castAddBody.embeds.reduce((acc: any, embed: any) => {
+export const getAlgoFeedFromAPI = async () => {
+  try {
+    const celebFollowing = await fetch(
+      `https://api.pinata.cloud/v3/farcaster/users?following=true&pageSize=100&fid=3`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.PINATA_JWT}`,
+        },
+      },
+    );
+
+    const followingData = await celebFollowing.json();
+
+    const users = followingData.data.users;
+    users.sort((a: any, b: any) => b.follower_count - a.follower_count);
+    const top30 = users.slice(0, 30);
+    let casts: any = [];
+    await Promise.all(
+      top30.map(async (user: any) => {
+        const userCasts = await fetch(
+          `https://api.pinata.cloud/v3/farcaster/casts?pageSize=10&fid=${user.fid}`,
+          {
+            headers: {
+              Authorization: `Bearer ${process.env.PINATA_JWT}`,
+            },
+          },
+        );
+        const castsData = await userCasts.json();
+        if (castsData.data.casts && castsData.data.casts.length > 0) {
+          casts.push(...castsData.data.casts);
+        }
+      }),
+    );
+
+    const filteredCasts = await Promise.all(
+      casts
+        .filter((cast: any) => cast !== undefined && !cast.parent_hash)
+        .map(async (cast: Cast) => {
+          const fname = cast.author?.username;
+          const pfp = cast.author?.pfp_url;
+          const { embedUrl, embedCast } = cast.embeds.reduce(
+            (acc: any, embed: Embed) => {
+              if (embed.url) {
+                acc.embedUrl.push(embed);
+              } else if (embed.castId) {
+                acc.embedCast.push(embed);
+              }
+              return acc;
+            },
+            { embedUrl: [], embedCast: [] },
+          );
+          return {
+            id: cast.hash,
+            castText: cast.content,
+            embedUrl: embedUrl,
+            embedCast: embedCast,
+            username: fname,
+            pfp: pfp,
+            timestamp: cast.timestamp,
+            likes: cast?.reactions?.likes?.length || 0,
+            recasts: cast?.reactions?.recasts?.length || 0,
+          };
+        }),
+    );
+    console.log(filteredCasts.length);
+    filteredCasts.sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime());
+
+    return filteredCasts;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+/* const simplifiedCasts = await Promise.all(
+      casts
+        .filter((c: Cast | undefined) => c && !c.parent_hash)
+        .map(async (cast: Cast) => {
+          const fname = cast.author?.username;
+          const pfp = cast.author?.pfp_url;
+          const { embedUrl, embedCast } = cast.embeds.reduce(
+            (acc: any, embed: Embed) => {
+              if (embed.url) {
+                acc.embedUrl.push(embed);
+              } else if (embed.castId) {
+                acc.embedCast.push(embed);
+              }
+              return acc;
+            },
+            { embedUrl: [], embedCast: [] },
+          );
+          return {
+            id: cast.hash,
+            castText: cast.content,
+            embedUrl: '',
+            embedCast: '',
+            username: fname,
+            pfp: pfp,
+            timestamp: cast.timestamp,
+            likes: cast?.reactions?.likes?.length || 0,
+            recasts: cast?.reactions?.recasts?.length || 0,
+          };
+        }),
+    );
+    return simplifiedCasts; */
+
+export const getFeed = async (channel: string, nextPage: string) => {
+  try {
+    const result = await fetch(
+      `${hubUrl}/castsByParent?url=${channel}&pageSize=20&reverse=true&pageToken=${nextPage}`,
+    );
+    const resultData = await result.json();
+    const pageToken = resultData.nextPageToken;
+    const casts = resultData.messages;
+    const simplifiedCasts = await Promise.all(
+      casts.map(async (cast: any) => {
+        const fname = await getFnameFromFid(cast.data.fid);
+        const pfp = await getPfpFromFid(cast.data.fid);
+        const { embedUrl, embedCast } = cast.data.castAddBody.embeds.reduce(
+          (acc: any, embed: any) => {
             if (embed.url) {
               acc.embedUrl.push(embed);
             } else if (embed.castId) {
               acc.embedCast.push(embed);
             }
             return acc;
-          }, { embedUrl: [], embedCast: [] });
-          return {
-            id: cast.hash,
-            castText: cast.data.castAddBody.text,
-            embedUrl: embedUrl,
-            embedCast: embedCast,
-            username: fname,
-            pfp: pfp,
-            timestamp: cast.data.timestamp,
-          };
-        }),
-      );
-      return simplifiedCasts;
-    } catch (error) {
-      console.log(error);
-      return error;
-    }
+          },
+          { embedUrl: [], embedCast: [] },
+        );
+        return {
+          id: cast.hash,
+          castText: cast.data.castAddBody.text,
+          embedUrl: embedUrl,
+          embedCast: embedCast,
+          username: fname,
+          pfp: pfp,
+          timestamp: cast.data.timestamp,
+        };
+      }),
+    );
+    return simplifiedCasts;
+  } catch (error) {
+    console.log(error);
+    return error;
   }
-  
+};
